70d4c5aecfdc6221b7f39598224584d4daa8a6c1
Julian Washingtion <cybermonkey1997@yahoo.com>
2017-10-11 12:11:42 -0400

    10/11/2017 Main Menu created

---
+++ BeatType/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+    //Toggleable states between a "Press Start" prompt, and the actual menu
+    public enum MenuState { startPrompt, menu   };
+    public MenuState currentMenu;
+
+    //Sprite object for the start prompt
+    public GameObject startPrompt;
+    //Sprite object for the menu buttons
+    public GameObject menuButtons;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //Scene initializes with the start prompt
+        currentMenu = MenuState.startPrompt;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(currentMenu == MenuState.startPrompt)
+        {
+            Instantiate(startPrompt);
+            Destroy(GameObject.Find("Menu Buttons"));
+        }
+
+        if(currentMenu == MenuState.menu)
+        {
+            Instantiate(menuButtons);
+            Destroy(GameObject.Find("Start Prompt"));
+        }
+	}
+}
