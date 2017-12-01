4a26e6ffbcae85d2ffe4a332fa816df6bb2b8466
Julian Washington <j_wash97@outlook.com>
2017-12-01 00:32:05 -0500

    11/30/2017 Functional Player character created, currently still in progress

---
+++ TheOccurance/Assets/Scripts/Flashlight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Hitting the 'F' key toggles the flashlight on and off
+        if (Input.GetKeyDown(KeyCode.F))
+            gameObject.GetComponent<Light>().enabled = !gameObject.GetComponent<Light>().enabled;
+	}
+}
