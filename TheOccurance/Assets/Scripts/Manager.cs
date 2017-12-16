b550da967d2559d1383273eae65e43fbf8503102
Logan <ll8875@rit.edu>
2017-12-16 18:29:52 -0500

    Readded changes

--- TheOccurance/Assets/Scripts/Manager.cs
+++ TheOccurance/Assets/Scripts/Manager.cs
@@ -20,6 +20,7 @@ public class Manager : MonoBehaviour
     //-----PRIVATE VARIABLES-----
     //Array of interestPoints
     interestPoint[] interPoints;
+    interestPoint[] sPoints;
     //Array of collectables
     GameObject[] collectables;
     //Player object
@@ -28,12 +29,18 @@ public class Manager : MonoBehaviour
     GameObject enemyObj;
     Enemy eScript;
     int collectedItems;
+    Text[] canvasTexts;
+    bool playerIsSafe;
 
     //-----PUBLIC VARIABLES-----
     //Public arrays to get the information for the interestPoints
     public Vector3[] locationPoints;
     public float[] dRads;
     public float[] sRads;
+
+    public Vector3[] safePoints;
+    public float safeDistance;
+
     public Terrain terrain;
     public TerrainData tData;
     public Canvas playerCanvas;
@@ -48,6 +55,9 @@ public class Manager : MonoBehaviour
 
     //Default radius to spawn the enemy
     const float DEFAULT_SRAD = 30.0f;
+
+    //Default radius of safe distance
+    const float DEFAULT_SAFE_DISTANCE = 15.0f;
 
     //-----GETTERS-----
     public TerrainData GetTerrainData
@@ -116,6 +126,23 @@ public class Manager : MonoBehaviour
             GameObject.Instantiate(markerPrefab, interPoints[i].point, Quaternion.identity);
         }
 
+        //Safe point setup
+        if(safeDistance <= 0)
+        {
+            safeDistance = DEFAULT_SAFE_DISTANCE;
+        }
+
+        sPoints = new interestPoint[safePoints.Length];
+
+        for (int i = 0; i < safePoints.Length; i++)
+        {
+            sPoints[i].detectRadius = safeDistance;
+            sPoints[i].inRange = false;
+            sPoints[i].point = safePoints[i];
+        }
+
+        playerIsSafe = false;
+
         //Get the player object
         playerObj = GameObject.FindGameObjectWithTag("Player");
 
@@ -125,57 +152,100 @@ public class Manager : MonoBehaviour
         eScript = enemyObj.GetComponent<Enemy>();
 
         collectables = GameObject.FindGameObjectsWithTag("Collectable");
-        playerCanvas.GetComponentInChildren<Text>().text = "Collected Items:  " + collectedItems + " / " + collectables.Length;
+        canvasTexts = playerCanvas.GetComponentsInChildren<Text>();
+        canvasTexts[0].text = "Collected Items:  " + collectedItems + " / " + collectables.Length;
         isPaused = false; 
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
         if (isPaused) Time.timeScale = 0; // Game paused  add code in this if statement for a pause menu 
         else Time.timeScale = 1; // game not paused 
         if (Input.GetKeyDown(KeyCode.Escape)) // toggle pause 
         {
             isPaused = !isPaused;
-            playerObj.GetComponent<FirstPersonController>().enabled = !playerObj.GetComponent<FirstPersonController>().enabled; 
+            playerObj.GetComponent<FirstPersonController>().enabled = !playerObj.GetComponent<FirstPersonController>().enabled;
         }
 
-            //Check win condition
-            if (collectedItems == collectables.Length)
+        //Check win condition
+        if (collectedItems == collectables.Length)
+        {
+            endGame();
+        }
+
+        //Detect how far away the player is from a point
+        for (int i = 0; i < interPoints.Length; i++)
+        {
+            Vector3 distanceVec = playerObj.transform.position - interPoints[i].point;
+            float distF = distanceVec.magnitude;
+
+            if (distF < interPoints[i].detectRadius && interPoints[i].inRange == false)
             {
-                endGame();
-            }
+                interPoints[i].inRange = true;
 
-            //Detect how far away the player is from a point
-            for (int i = 0; i < interPoints.Length; i++)
-            {
-                Vector3 distanceVec = playerObj.transform.position - interPoints[i].point;
-                float distF = distanceVec.magnitude;
-
-                if (distF < interPoints[i].detectRadius && interPoints[i].inRange == false)
+                if (eScript.EState != Enemy.EnemyStates.Chasing)
                 {
-                    interPoints[i].inRange = true;
-
-                    if (eScript.Chasing == false)
-                    {
-                        eScript.CircularTeleportTo(interPoints[i].point, interPoints[i].spawnRadius);
-                    }
-
-                }
-                else if (distF > interPoints[i].detectRadius && interPoints[i].inRange == true)
-                {
-                    interPoints[i].inRange = false;
+                    eScript.CircularTeleportTo(interPoints[i].point, interPoints[i].spawnRadius);
                 }
 
             }
+            else if (distF > interPoints[i].detectRadius && interPoints[i].inRange == true)
+            {
+                interPoints[i].inRange = false;
+            }
+
+        }
+
         
+        for(int i = 0; i < sPoints.Length; i++)
+        {
+            //Detect player distance from a safe point
+            Vector3 distanceVec = playerObj.transform.position - sPoints[i].point;
+            float distF = distanceVec.magnitude;
+
+            if (distF < safeDistance)
+            {
+                if(sPoints[i].inRange == false)
+                {
+                    sPoints[i].inRange = true;
+                }
+
+                if (eScript.EState == Enemy.EnemyStates.Chasing)
+                {
+                    eScript.EState = Enemy.EnemyStates.Retreating;
+                }
+
+            }
+            else if (distF > safeDistance)
+            {
+                if(sPoints[i].inRange)
+                {
+                    sPoints[i].inRange = false;
+                }
+            }
+            //Detect enemy distance from a safe point
+            distanceVec = enemyObj.transform.position - sPoints[i].point;
+            distF = distanceVec.magnitude;
+
+            if (distF < safeDistance)
+            {
+                eScript.EState = Enemy.EnemyStates.Retreating;
+            }
+        }
+
+        //Text fade
+        if (Time.timeSinceLevelLoad > 3.0f)
+        {
+            canvasTexts[1].CrossFadeAlpha(0.0f, 1.0f, false);
+        }
 	}
 
     //Increases the points for collected items
     public void IncreaseCollectedItems()
     {
         collectedItems++;
-        playerCanvas.GetComponentInChildren<Text>().text = "Collected Items:  " + collectedItems + " / " + collectables.Length;
+        canvasTexts[0].text = "Collected Items:  " + collectedItems + " / " + collectables.Length;
     }
 
     //Return the game to the title screen or something
